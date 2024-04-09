0762e1826b27b947e1612c57949c18c0b60354f0
Benzel <benawhiting@gmail.com>
2024-04-09 15:28:17 -0700

    Make DataAccess methods async

--- DataLibrary/IDataAccess.cs
+++ DataLibrary/IDataAccess.cs
@@ -1,9 +1,8 @@
-﻿
-namespace DataLibrary
+﻿namespace DataLibrary
 {
     public interface IDataAccess
     {
-        List<T> LoadData<T, U>(string sql, U parameters, string connectionString);
-        void SaveData<T>(string sql, T parameters, string connectionString);
+        Task<List<T>> LoadData<T, U>(string sql, U parameters, string connectionString);
+        Task SaveData<T>(string sql, T parameters, string connectionString);
     }
 }
