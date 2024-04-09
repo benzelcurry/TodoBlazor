59d4782997c27d630f12cf268ca91971b1632928
Benzel <benawhiting@gmail.com>
2024-04-09 15:00:59 -0700

    Add a method for saving data

--- DataLibrary/DataAccess.cs
+++ DataLibrary/DataAccess.cs
@@ -6,13 +6,21 @@ namespace DataLibrary
 {
     public class DataAccess
     {
-        public static List<T> LoadData<T, U>(string sql, U parameters, string connectionString)
+        public List<T> LoadData<T, U>(string sql, U parameters, string connectionString)
         {
             using (IDbConnection connection = new MySqlConnection(connectionString))
             {
                 List<T> rows = connection.Query<T>(sql, parameters).ToList();
 
                 return rows;
+            }
+        }
+
+        public void SaveData<T>(string sql, T parameters, string connectionString)
+        {
+            using (IDbConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Execute(sql, parameters);
             }
         }
     }
