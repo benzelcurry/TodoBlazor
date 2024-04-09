673d7f3c0daaa519944319eefcd67ef0a78282a1
Benzel <benawhiting@gmail.com>
2024-04-09 14:57:49 -0700

    Add a DataAccess class with a method for loading data

---
+++ DataLibrary/DataAccess.cs
@@ -0,0 +1,19 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace DataLibrary
+{
+    public class DataAccess
+    {
+        public static List<T> LoadData<T, U>(string sql, U parameters, string connectionString)
+        {
+            using (IDbConnection connection = new MySqlConnection(connectionString))
+            {
+                List<T> rows = connection.Query<T>(sql, parameters).ToList();
+
+                return rows;
+            }
+        }
+    }
+}
